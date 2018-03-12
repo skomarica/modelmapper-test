abb339268d9ea2f9e06be43a24af20981dcb5b04
Sinisa Komarica <sinisa.komarica@united.cloud>
2018-03-12 22:17:59 +0100

    Added passing/failing tests

--- src/test/java/io/github/skomarica/practice/mapping/MappingTest.java
+++ src/test/java/io/github/skomarica/practice/mapping/MappingTest.java
@@ -14,7 +14,23 @@ import static org.modelmapper.convention.MatchingStrategies.STRICT;
 public class MappingTest {
 
     @Test
-    public void b() {
+    public void passing() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(STRICT);
+
+//        modelMapper.map(new SimpleEntity(), new SimpleEntity());
+
+        final ComplexEntity newEntity = new ComplexEntity(1, null);
+        final ComplexEntity previousEntity = new ComplexEntity(2, new SimpleEntity(3L, "3 Name"));
+
+        modelMapper.map(newEntity, previousEntity);
+
+        assertThat(previousEntity.getPosition()).isEqualTo(1);
+        assertThat(previousEntity.getSimpleEntity()).isNull();
+    }
+
+    @Test
+    public void failing() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(STRICT);
 
